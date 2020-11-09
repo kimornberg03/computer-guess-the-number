e716cf3fe51612e3dc41d913f1a92820faac4d7c
unknown <kim.ornberg@skola.botkyrka.se>
2020-11-09 15:56:20 +0100

    computer-guess

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const MAX_NUMBER = 100
-const MIN_NUMBER = 0
+let MAX_NUMBER = 100
+let MIN_NUMBER = 0
 
 let guess
 
-document.getElementById('too-high-btn').addEventListener('click', function () {
-    // Vad ska hända när man tryckt too high?
+document.getElementById('too-high-btn').addEventListener('click', function() {
+    setMessage(`Is it ${guess/2 + guess}?`)
+        // Vad ska hända när man tryckt too high?
 
 })
 
-document.getElementById('too-low-btn').addEventListener('click', function () {
-    // Vad ska hända när man tryckt too low?
-    
+document.getElementById('too-low-btn').addEventListener('click', function() {
+    setMessage(`Is it ${guess/2}?`)
+        // Vad ska hända när man tryckt too low?
+
+})
+
+document.getElementById('is-correct-btn').addEventListener('click', function() {
+    setMessage("WOHO! ;)")
+        // Vad ska hända när man tryckt too low?
+
 })
 
 
@@ -22,12 +30,11 @@ function start() {
     document.getElementById('before-start').style.display = 'none'
     document.querySelector('main').style.display = 'block'
 
-    guess = 0
+    guess = 50
 
-    // setMessage(`Is it ${guess}?`)
+    setMessage(`Is it ${guess}?`)
 }
 
 document
     .getElementById('start-btn')
     .addEventListener('click', start)
-
